aa11039e3a797a7f0ff84febd51c37b821e294b8
Alejandro Perez <alecio88@gmail.com>
2022-09-26 07:29:29 -0300

    Adds Validator

--- src/subscriptions/dto/create-subscription.dto.ts
+++ src/subscriptions/dto/create-subscription.dto.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateSubscriptionDto {
 
+  @IsNotEmpty()
   token: string;
+  
+  @IsNotEmpty()
   expire_date: Date;
+  
+  @IsNotEmpty()
   issue_date: Date;
+
+  @IsEmail()
+  @IsNotEmpty()
   user_email: string;
+  
+  @IsNotEmpty()
   user_slug: string;
 }
