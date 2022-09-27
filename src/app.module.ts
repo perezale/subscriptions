7988dbc686718354c2b7551be59c30ba062494a4
Alejandro Perez <alecio88@gmail.com>
2022-09-26 23:27:43 -0300

    Add DB Environmental Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,19 @@ import { AppService } from './app.service';
 import { Subscription } from './subscriptions/entities/subscription.entity';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
+const db_options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+}
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'secret123!',
-      database: 'subscriptions',
+      ...db_options,
       entities: [Subscription],
       synchronize: true,
     }),
