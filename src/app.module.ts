25dc40468336ce137038b348eb75748ee86596d0
Alejandro Perez <alecio88@gmail.com>
2022-09-27 00:11:06 -0300

    fix db user typeorm attribute

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ require('dotenv').config()
 const db_options = {
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
-  user: process.env.DB_USER,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME, 
 }
