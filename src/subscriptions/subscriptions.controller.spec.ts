b8542ce874b802d6930764e9b725a34010d15400
Alejandro Perez <alecio88@gmail.com>
2022-10-06 14:10:42 -0300

    Add Basic Repository mocks to tests

--- src/subscriptions/subscriptions.controller.spec.ts
+++ src/subscriptions/subscriptions.controller.spec.ts
@@ -1,17 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Subscription } from './entities/subscription.entity';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
+import { MockType, repositoryMockFactory } from './subscriptions.service.spec';
 
 describe('SubscriptionsController', () => {
   let controller: SubscriptionsController;
+  let repositoryMock: MockType<Repository<Subscription>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SubscriptionsController],
-      providers: [SubscriptionsService],
+      providers: [SubscriptionsService,
+        {
+          provide: getRepositoryToken(Subscription),
+          useFactory: repositoryMockFactory
+        }],
     }).compile();
 
     controller = module.get<SubscriptionsController>(SubscriptionsController);
+    repositoryMock = module.get(getRepositoryToken(Subscription));
   });
 
   it('should be defined', () => {
--- src/subscriptions/subscriptions.service.spec.ts
+++ src/subscriptions/subscriptions.service.spec.ts
@@ -1,15 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Subscription } from './entities/subscription.entity';
 import { SubscriptionsService } from './subscriptions.service';
+
+// @ts-ignore
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+  findOne: jest.fn(entity => entity),
+  // ...
+}));
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
 
 describe('SubscriptionsService', () => {
   let service: SubscriptionsService;
+  let repositoryMock: MockType<Repository<Subscription>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SubscriptionsService],
+      providers: [SubscriptionsService,
+        {
+          provide: getRepositoryToken(Subscription),
+          useFactory: repositoryMockFactory
+        }],
     }).compile();
 
     service = module.get<SubscriptionsService>(SubscriptionsService);
+    repositoryMock = module.get(getRepositoryToken(Subscription));
   });
 
   it('should be defined', () => {
